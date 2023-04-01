bfce7f7ad7318c46187f990bb9e3edfb0c3d229f
barmal <barmal.1992@wp.pl>
2023-04-01 14:23:00 +0200

    Added few docstring descriptions

--- sudoku_obj.py
+++ sudoku_obj.py
@@ -1,14 +1,20 @@
 class Sudoku:
     def __init__(self, user_input):
+        """
+        Initialize the sudoku table
+        :param user_input: list of fields as int: xyz
+            x - position top-bot (1-9)
+            y - position l-r (1-9)
+            z - value (1-9)
+        """
+
         print("<" * 20, "SUDOKU INITIALIZATION", ">" * 20)
         self.array = []
         self.fillers = []
 
         for _ in range(9):
-            line_filler = []
             self.array.append([" "] * 9)
-            for _ in range(9):
-                line_filler.append(set(map(lambda x: x + 1, range(9))))
+            line_filler = [set(range(1, 10)) for _ in range(9)]
             self.fillers.append(line_filler)
         self.numeration_help()
         self.import_values(*user_input)
@@ -23,8 +29,10 @@ class Sudoku:
         return self.printer
 
     def show(self):
-        print("\nCurrent state of the sudoku")
-        print("_" * 25)
+        """
+        Print the sudoku table
+        """
+        print("\nCurrent state of the sudoku\n" + "_" * 25)
         for i, line in enumerate(self.array):
             print("| ", end="")
             for j, num in enumerate(line):
@@ -38,11 +46,19 @@ class Sudoku:
             print()
 
     def show_vals_fillers(self):
+        """
+        Print:
+         val - value that each field possesses
+         filler - set of possible values for the field
+        """
         for i in range(9):
             for j in range(9):
                 print("Val: ", self.array[i][j], "Filler: ", self.fillers[i][j])
 
     def show_fillers(self):
+        """
+        Print the possible values for each field in the style of a table
+        """
         for i, line in enumerate(self.fillers):
             if not i % 3:
                 print("-"*117)
@@ -58,10 +74,24 @@ class Sudoku:
         print("\nSudoku created!")
 
     def add_value(self, val: tuple):
+        """
+        Add value for a field
+        :param val:  as int: xyz
+            x - position top-bot (1-9)
+            y - position l-r (1-9)
+            z - value (1-9)
+        """
         print(f"Insert value: row {val[0] + 1}, col {val[1] + 1}, num {val[2]}")
         self.array[val[0]][val[1]] = val[2]
 
     def import_values(self, *args):
+        """
+        :param args: list of fields as int: xyz
+            x - position top-bot (1-9)
+            y - position l-r (1-9)
+            z - value (1-9)
+        """
+
         for arg in args:
             next_val = str(arg)
             next_val = tuple([int(next_val[0]) - 1, int(next_val[1]) - 1, int(next_val[2])])
@@ -73,6 +103,10 @@ class Sudoku:
         self.add_value(val)
 
     def init_fillers(self):
+        """
+        If a field has a value, then update its possible values to have only this one
+        :return:
+        """
         print("\n1. If value known, leave ONLY field value in its fillers set(). Repeat for all fields")
         for i, line in enumerate(self.array):
             for j, field in enumerate(line):
@@ -80,6 +114,11 @@ class Sudoku:
                     self.fillers[i][j].intersection_update({field})
 
     def find_the_cross(self, field_num):
+        """
+        Find the defined values (without " ") that are in one line with input field left-right and top-bottom
+        Field included, but if searched for a field " ", then logically excluded
+        :return: Return them as one set
+        """
         tb, lr = [], []  # top-bottom, left-right
 
         for i in range(9):
@@ -90,6 +129,11 @@ class Sudoku:
         return cross
 
     def find_the_box(self, field_num):
+        """
+        Find the defined values (without " ") that are in one box 3x3 with input field (sudoku has 9 boxes 3x3)
+        Field included, but if searched for a field " ", then logically excluded
+        :return: Return them as one set
+        """
         box_ids = dict()
         box = set()
         for i in range(3):
@@ -106,6 +150,10 @@ class Sudoku:
         return box
 
     def cross_fillers(self):
+        """
+
+        :return:
+        """
         print("2. Remove from field filler values that are written in top-bot left-right fields (cross)")
         print("If only one filler element and the array value is ' ', then it'll be updated with the filler")
 
@@ -202,7 +250,9 @@ class Sudoku:
             self.fillers[field_num[0]][field_num[1]].intersection_update(temp)
 
     def advanced_search(self):
-        # Fill with ideas for improvement
+        # hidden pair
+        # hidden three
+        # swordfish method
         pass
 
     def is_done(self):
